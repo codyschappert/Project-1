0f585cbd90d0c506e01e0517b26aa583b6898608
codyschappert <codyschappert1223@gmail.com>
2025-12-02 13:23:56 -0600

    added vote submitted popup

--- logic.py
+++ logic.py
@@ -1,5 +1,6 @@
 from badidgui import *
 from votemenugui import *
+from votesubmittedgui import *
 from PyQt6.QtWidgets import *
 from alreadyvotederrorgui import *
 
@@ -15,17 +16,10 @@ class Logic(QMainWindow, Ui_MainWindow):
 
     def submit(self):
         user_id = self.ID_input.text()
-        if self.confirm_id(user_id):
-            print(ID_list)
-        elif not self.confirm_id(user_id):
-            pass
-
-    def confirm_id(self, id_num) -> bool | None:
         try:
-            id_num = int(id_num)
+            id_num = int(user_id)
             if id_num not in ID_list:
                 ID_list.append(id_num)
-                return True
             elif id_num in ID_list:
                 raise TypeError("Invalid ID")
         except ValueError:
@@ -48,6 +42,14 @@ class AlreadyVoted(QDialog, Ui_Dialog):
         super().__init__()
         self.setupUi(self)
         self.close_error.clicked.connect(lambda: self.close_popup())
+
+    def close_popup(self):
+        self.close()
+
+class VoteSubmitted(QDialog, VoteSubmitted):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
     def close_popup(self):
         self.close()
